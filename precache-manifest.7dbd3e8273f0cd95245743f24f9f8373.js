1fca8542fe6fef4f241cfa8c717711a55671f81c
jazminereyes <jaaaaazreyes@gmail.com>
2020-08-22 11:59:23 +0800

    Updates

---
+++ precache-manifest.7dbd3e8273f0cd95245743f24f9f8373.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d595d16741b2f64b4f68027009292c9",
+    "url": "/communique/index.html"
+  },
+  {
+    "revision": "423a014e92e35f643201",
+    "url": "/communique/static/css/main.979ecff4.chunk.css"
+  },
+  {
+    "revision": "cf5676a6b15d6fc7ac11",
+    "url": "/communique/static/js/2.b8b382d6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/communique/static/js/2.b8b382d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "423a014e92e35f643201",
+    "url": "/communique/static/js/main.76d6bb7f.chunk.js"
+  },
+  {
+    "revision": "fbaf850628a6c7ebf739",
+    "url": "/communique/static/js/runtime-main.39350fb2.js"
+  }
+]);
